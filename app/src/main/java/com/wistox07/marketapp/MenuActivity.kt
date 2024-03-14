e7dd22a9edf6ae89bf4053f360f4db5f1aa4cf8c
wistox07 <fodelosrios@gmail.com>
2024-03-14 07:47:02 -0500

    Fragment visual

---
+++ app/src/main/java/com/wistox07/marketapp/MenuActivity.kt
@@ -0,0 +1,17 @@
+package com.wistox07.marketapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        //val btnTab1 : Button = findViewById(R.id.btnTab1)
+        //ViewBIndings
+
+
+    }
+}
