824c75d4f07ef29bc90e06e3f3924f0f88e2fdc8
wistox07 <fodelosrios@gmail.com>
2024-03-15 08:03:01 -0500

    Implementacion Fragments

--- app/src/main/java/com/wistox07/marketapp/MenuActivity.kt
+++ app/src/main/java/com/wistox07/marketapp/MenuActivity.kt
@@ -3,15 +3,39 @@ package com.wistox07.marketapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.wistox07.marketapp.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+    //ActivityRegisterBinding
+
+    //private val binding : ActivityMenuBinding? = null
+    private lateinit var binding : ActivityMenuBinding
+    private lateinit var fragment : Fragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_menu)
 
         //val btnTab1 : Button = findViewById(R.id.btnTab1)
         //ViewBIndings
+        binding.btnTab1.setOnClickListener {
+            fragment = TabOneFragment()
+            addFragment()
+        }
 
+        binding.btnTab1.setOnClickListener {
+            fragment = TabTwoFragment()
+            addFragment()
+        }
+
+
+    }
+    private fun addFragment(){
+        fragment?.let {
+            supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+        }
 
     }
 }
