319cc706d04f3d0c312780bd68d3527e7abf406d
wistox07 <fodelosrios@gmail.com>
2024-03-27 07:48:50 -0500

    Login dev

--- app/src/main/java/presentation/login/LoginFragment.kt
+++ app/src/main/java/presentation/login/LoginFragment.kt
@@ -21,12 +21,13 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
-
+    /*
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Handler(Looper.getMainLooper()).postDelayed({
                 Navigation.findNavController(view).navigate(R.id.action_splashFragment_to_loginFragment3)
         }, SPLASH_TIME_OUT)
     }
+    */
 
 }
